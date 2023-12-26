82c5510c05c9bfd2384c425f153f27e60eaf53ae
shekharjoshi <shekharjoshi813@gmail.com>
2023-12-26 17:33:30 +0530

    basic info

---
+++ script.js
@@ -0,0 +1,7 @@
+const About = document.getElementById('About')
+const liAbout = document.getElementById('liAbout')
+
+liAbout.addEventListener('click',()=>{
+    console.log('pallu')
+    About.classList.add('Show')
+})
