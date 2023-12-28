454b1fbb457e3909e809534923d817c44a04789f
shekharjoshi <shekharjoshi813@gmail.com>
2023-12-28 14:51:37 +0530

    animate every section

--- script.js
+++ script.js
@@ -1,13 +1,72 @@
+const Home = document.getElementById('Home')
 const About = document.getElementById('About')
-const liAbout = document.getElementById('liAbout')
+const Project = document.getElementById('Project')
+const Certificate = document.getElementById('Certificate')
+const Contact = document.getElementById('Contact')
 
-liAbout.addEventListener('click',()=>{
-    console.log('pallu')
-    About.classList.add('Show')
+
+
+const liHome = document.getElementById('liHome')
+const liAbout = document.getElementById('liAbout')
+const liProject = document.getElementById('liProject')
+const liCertificate = document.getElementById('liCertificate')
+const liContact = document.getElementById('liContact')
+const touch = document.getElementById('touch')
+
+liHome.addEventListener('click',()=>{
+    Home.classList.add('Show')
+    About.classList.remove('Show')
+    Project.classList.remove('Show')
+    Certificate.classList.remove('Show')
+    Contact.classList.remove('Show')
+    
+
 })
+liAbout.addEventListener('click',()=>{
+    About.classList.add('Show')
+    Home.classList.remove('Show')
+    Project.classList.remove('Show')
+    Certificate.classList.remove('Show')
+    Contact.classList.remove('Show')
+
+})
+liProject.addEventListener('click',()=>{
+    Project.classList.add('Show')
+    Home.classList.remove('Show')
+    About.classList.remove('Show')
+    Certificate.classList.remove('Show')
+    Contact.classList.remove('Show')
+
+})
+liCertificate.addEventListener('click',()=>{
+    Certificate.classList.add('Show')
+    Home.classList.remove('Show')
+    About.classList.remove('Show')
+    Project.classList.remove('Show')
+    Contact.classList.remove('Show')
+
+})
+liContact.addEventListener('click',()=>{
+    Contact.classList.add('Show')
+    Home.classList.remove('Show')
+    About.classList.remove('Show')
+    Project.classList.remove('Show')
+    Certificate.classList.remove('Show')
+
+})
+touch.addEventListener('click',()=>{
+    Contact.classList.add('Show')
+    Home.classList.remove('Show')
+    About.classList.remove('Show')
+    Project.classList.remove('Show')
+    Certificate.classList.remove('Show')
+
+})
+
+
 var typed = new Typed('#element', {
     strings: ["Hello, my name is Palash Khandelwal. I am a passionate individual with a strong desire to learn and grow in the technical field. I believe that constant learning and staying updated with the latest developments in the field are essential for personal and professional growth. I am always eager to explore new technologies, expand my knowledge base, and apply my skills in practical scenarios."],
-    typeSpeed: 20,
+    typeSpeed: 10,
     smartBackspace: true,
     backDelay: 1000,
     loop:false,
@@ -16,4 +75,4 @@ var typed = new Typed('#element', {
   let Touch=document.getElementById('touch');
   setTimeout(() => {
     Touch.classList.add('btn')
-  }, "16000");
+  }, "7700");
